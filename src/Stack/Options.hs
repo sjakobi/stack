f6c426d1d4ab9bc314248adfdb6f7b306398baea
Simon Jakobi <simon.jakobi@gmail.com>
2016-06-14 03:27:51 +0200

    Simplify readAbsDir to `eitherReader' parseAbsDir`

--- src/Stack/Options.hs
+++ src/Stack/Options.hs
@@ -223,7 +223,7 @@ configOptsParser hide0 =
         , configMonoidModifyCodePage = modifyCodePage
         , configMonoidAllowDifferentUser = allowDifferentUser
         })
-    <$> optionalFirst (option readAbsDir
+    <$> optionalFirst (option (eitherReader' parseAbsDir)
             ( long stackRootOptionName
             <> metavar (map toUpper stackRootOptionName)
             <> help ("Absolute path to the global stack root directory " ++
@@ -303,15 +303,6 @@ configOptsParser hide0 =
                 "directory to use a stack installation (POSIX only)")
             hide
   where hide = hideMods (hide0 /= OuterGlobalOpts)
-
-readAbsDir :: ReadM (Path Abs Dir)
-readAbsDir = do
-    s <- readerAsk
-    case parseAbsDir s of
-        Just p -> return p
-        Nothing ->
-            readerError
-                ("Failed to parse absolute path to directory: '" ++ s ++ "'")
 
 buildOptsMonoidParser :: Bool -> Parser BuildOptsMonoid
 buildOptsMonoidParser hide0 =
